d3973a398085f3afa7331dd4ab49812d72d1ed32
nirgunrohan <rohannirgun69@gmail.com>
2025-03-12 01:18:48 +0530

    Chapter 4 Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Video from "./components/Video";
 import "./App.css";
 import videos from "./data/data.js";
+import PlayButton from "./components/PlayButton/PlayButton.js";
+
+
 function App() {
   
   return (
-    <div className="App">
+    <div className="App" onClick={()=>console.log('App')}>
       <div>Videos</div>
       {videos.map((video) => (
         <Video
@@ -14,8 +17,22 @@ function App() {
           views={video.views}
           time={video.time}
           channel={video.channel}
-        ></Video>
+        >
+          <PlayButton 
+            onPlay={()=>console.log('Playing',video.title)} 
+            onPause={()=>console.log('Paused',video.title)}
+          >
+            {video.title}
+          </PlayButton>
+        </Video>
       ))}
+
+      <div style={{clear:"both"}}>
+      {/*<PlayButton message="play-msg" onPlay={()=>console.log('Play')} onPause={()=>console.log('Paused')}>Play</PlayButton>*/}
+      {/*<PlayButton message="pause-msg" onSmash={()=>alert('Play')}>Pause</PlayButton>*/}
+      </div>
+      
+
     </div>
   );
 }
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,21 +1,27 @@
-import './Video.css';
+import "./Video.css";
 
-function Video({title, channel, views, time,verified}) {
-   
-    return <>
-    <div className="container">
-        <div className='pic'>
-            <img src="https://placebear.com/1280/720" alt="Cat"></img>
+function Video({ title, channel="Coder Rohan", views, time, verified, children }) {
+  return (
+    <>
+      <div className="container">
+        <div className="pic">
+          <img src="https://placebear.com/1280/720" alt="Cat"></img>
         </div>
         <div className="title">{title}</div>
-        <div className="channel">{channel} {verified && '✅'}</div> 
-        <div className="views">
-            {title}
-            {views} views <span>.</span> 
-            {time}
+        <div className="channel">
+          {channel} {verified && "✅"}
         </div>
-    </div>
+        <div className="views">
+          {title}
+          {views} views <span>.</span>
+          {time}
+        </div>
+        <div>
+          {children}
+        </div>
+      </div>
     </>
+  );
 }
 
 export default Video;
